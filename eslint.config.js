d19f33ca1c6bff4b73a3d7b3cc58d92f33c2fef7
Ed Preston <epreston@prestonsoft.com>
2026-02-21 08:44:53 +1100

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import sharedConfig from '@epreston/eslint-config';
+
+/** @type { import('eslint').Linter.Config[] } */
+export default [
+  ...sharedConfig
+  // {
+  //   // ignores must be completely separate from other rules
+  //   name: 'project/ignores',
+  //   ignores: ['**/packages/*/lib', '**/packages/*/inc']
+  // },
+  // {
+  //   name: 'project/rules',
+  //   rules: {
+  //     'no-console': 'off'
+  //   }
+  // }
+];
