4115cece7debc72a8c524b19cb512530ffd88493
Ed Preston <epreston@prestonsoft.com>
2026-02-22 05:55:44 +1100

    Add vitest

---
+++ vitest.config.js
@@ -0,0 +1,31 @@
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
+
+import viteConfig from './vite.config.js';
+
+// https://vitejs.dev/config/
+const testConfig = defineConfig({
+  test: {
+    // Debugging Tests
+    // testTimeout: 0,
+    // fileParallelism: false,
+
+    // globals: true,
+    // includeSource: ['src/**/*.{js,ts}'], // enable inline tests
+    pool: 'forks',
+    setupFiles: ['./scripts/setup-vitest.js'],
+    sequence: {
+      hooks: 'list'
+    },
+    coverage: {
+      provider: 'v8',
+      reporter: ['text-summary', 'html'],
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        // entries that skew coverage reports here
+        'scripts/**'
+      ]
+    }
+  }
+});
+
+export default mergeConfig(viteConfig, testConfig);
